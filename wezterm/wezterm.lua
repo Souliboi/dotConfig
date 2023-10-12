5e9fa50e7204fe34780055ff5775f917833bd967
Souliboi <106060704+Souliboi@users.noreply.github.com>
2023-10-13 00:28:51 +0200

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,45 @@
 local wezterm = require 'wezterm'
 local config = {}
 if wezterm.config_builder then
-   config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" })
 config.font_size = 10
+config.window_frame = {
+	font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" }),
+	font_size = 10,
+	active_titlebar_bg = '#262626',
+	inactive_titlebar_bg = '#262626',
+}
+config.colors = {
+	foreground = '#C0C0C0',
+	background = '#262626',
+	ansi = {
+		'#232627',
+		'#ED1515',
+		'#11d116',
+		'#F67400',
+		'#1D99F3',
+		'#9B59B6',
+		'#1ABC9C',
+		'#FCFCFC',
+
+	},
+	brights = {
+		'#7F8C8D',
+		'#C0392B',
+		'#1CDC9A',
+		'#FDBC4B',
+		'#3DAEE9',
+		'#8E44AD',
+		'#16A085',
+		'#FFFFFF',
+
+	},
+	tab_bar = {
+		inactive_tab_edge = '#CCCCCC',
+	},
+}
 
 return config
