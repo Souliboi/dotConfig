fb10ab2297d79cc8b7e65b8108c57098f73b9161
Souliboi <106060704+Souliboi@users.noreply.github.com>
2023-10-12 03:07:42 +0200

    neovim updates

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,4 +1,8 @@
 local lsp = require("lsp-zero")
 
-lsp.preset('recommended')
-lsp.setup()
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('lspconfig').lua_ls.setup({})
+require('lspconfig').nil_ls.setup({})
